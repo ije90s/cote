e7386f393950a4beb809f20a8910896fdde4cc28
ije90s <idgocm@gmail.com>
2022-08-31 21:04:58 +0900

    6단계 끝

--- boj/step6/p1316.js
+++ boj/step6/p1316.js
@@ -5,11 +5,18 @@ let groupChar = 0;
 for(let i=1;i<=count;i++){
    
     const str = input[i];
+    let isCheck = true;
     for(let j = 0; j < str.length; j++){
-        const index = str.indexOf(str[j]); 
-        
+        const current = str[j]; //현재 문자
+        const currentIndex = str.indexOf(current, j); //현재 인덱스값
+        const nextIndex = str.indexOf(current, j+1); //다음 인덱스값
+
+        //같은 문자가 있는 경우, 바로 다음 인덱스값이 아닌 경우
+        if(nextIndex > -1 && nextIndex-currentIndex > 1) isCheck = false;
     }
 
+    //그룹 단어인 경우에만 증가
+    if(isCheck) groupChar++;
 }
 
 console.log(groupChar);
--- boj/step6/p2941.js
+++ boj/step6/p2941.js
@@ -1,7 +1,10 @@
 const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().trim();
-const croatia = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z='];
 
-for(key in croatia){
-    if(key.in)
-}
+const list = 'c=|c-|dz=|d-|lj|nj|s=|z=';
+const regexp = new RegExp(list, "g"); //정규식으로 선언
+const replaced = input.replace(regexp, "*"); //정규식에 포함되어 있으면 *로 변경
+
+console.log(replaced.length);
+
+//참조: https://developer.mozilla.org/ko/docs/Web/JavaScript/Guide/Regular_Expressions
